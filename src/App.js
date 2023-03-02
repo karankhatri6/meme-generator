a7760ea3f7ed622d2292a1fd5f8f452ac7a6c9c6
Karan Khatri <karankhatri535@gmail.com>
2023-03-02 23:54:17 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Meme from './components/Meme';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Meme />
     </div>
   );
 };
